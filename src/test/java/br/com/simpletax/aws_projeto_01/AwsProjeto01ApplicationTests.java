f1b1cdca2c409169a0e18ce08c545e65666c00fe
Eduardo Lucio <eduardomalone@hotmail.com>
2024-02-28 14:43:40 -0300

    primeira subida

---
+++ src/test/java/br/com/simpletax/aws_projeto_01/AwsProjeto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simpletax.aws_projeto_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjeto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
